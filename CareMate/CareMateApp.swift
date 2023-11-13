50d9bd6fb046fea8caf9799b9c28511ee0c8cbc4
Soren Marcelino <soren.marcelino@icloud.com>
2023-11-13 19:30:18 +0100

    Initial Commit

---
+++ CareMate/CareMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareMateApp.swift
+//  CareMate
+//
+//  Created by Soren Marcelino on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CareMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
